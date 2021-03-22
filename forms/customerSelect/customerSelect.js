8f522da56bdc536bfbcab11a9b77483cd72aa275
DBSunderland <davissunderland@creighton.edu>
2021-03-22 15:02:51 -0500

    15:02 update

--- forms/customerSelect/customerSelect.js
+++ forms/customerSelect/customerSelect.js
@@ -1,23 +1,28 @@
-query = "SELECT * FROM customer"
-req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+let query = ""
+let req = {}
+let results = []
+let pw = "Albatro$$19"
+let netID = "dbs15065"
 
-btnShowCustomers.onload=function(){
-    query = "SELECT * FROM customer"
-    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
-}
 
-if (req.status == 200) {
-  results = JSON.parse(req.responseText)
-  if (results.length == 0) {
-    lblMessage1.textContent = "There are no pets in the database."
-} else {
-    let message = ""
-    for (i = 0; i < results.length; i++)
-      message = message + results[i][1] + "\n"
-    txtResults.value = message
+customerSelect.onshow = function() {
+  query = "SELECT * FROM customer"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+  if (req.status == 200) {
+    results = JSON.parse(req.responseText)
+    console.log(`The results are \n ${results}`)
+    if (results.length == 0)
+      lblMessage.value = "There are no customers in the database."
+    else {
+      let message = ""
+      for (i = 0; i < results.length; i++)
+        message = message + results[i][1] + "\n"
+      txtResults.value = message
     }
+  } else
+    lblMessage.value = "Error code: " + req.status
 }
-
 
 
 /*  SELECT CODE Cleaned up
--- forms/overview/overview.js
+++ forms/overview/overview.js
@@ -1,8 +1,3 @@
 // global variables for database calls
-let req = ""
-let query = ""
-let results = ""
-let pw = "Albatro$$19"
-let netID = "dbs15065"
-let allPetData = []
+
 
